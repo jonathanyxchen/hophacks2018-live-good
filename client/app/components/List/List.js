e0a6effde43964a7f8b1b981c1eaab77372588b3
shawnchan2014 <chenzx.shawn2013@gmail.com>
2018-09-15 18:31:29 -0400

    Add List and Map

---
+++ client/app/components/List/List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class ListView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <p>List</p>
+    )
+  }
+}
+
+export default ListView;
---
+++ client/app/components/Map/Map.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class MapView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <p>Map</p>
+    )
+  }
+}
+
+export default MapView;
